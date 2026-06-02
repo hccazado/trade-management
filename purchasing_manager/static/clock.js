a7badd3b228be43c616f86ac12159746f78b5fd1
hccazado <heitorcazado@gmail.com>
2026-06-02 13:06:35 -0300

    adjusting general layout to improve mobile usage

--- purchasing_manager/static/clock.js
+++ purchasing_manager/static/clock.js
@@ -1,32 +1,19 @@
-window.addEventListener("load", ()=>{
-    
+window.addEventListener("load", () => {
+
     startTime(clock)
 })
 
 function startTime() {
-    
+
     clock = document.getElementById("clock")
 
-    const today = new Date();
+    const now = new Date();
 
-    let d = today.getDate();
-
-    let mo = today.getMonth() + 1;
-
-    let h = today.getHours();
-
-    let m = today.getMinutes();
-
-    m = checkTime(m);
-
-    clock.innerHTML = d + " / " + mo + " - " + h + ":" + m;
-    
-    setTimeout(startTime, 1000);
+    clock.innerHTML = "Data: " + now.toLocaleString('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    }).replace(",", " -");
 }
-
-function checkTime(i) {
-
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    
-    return i;
-  }
